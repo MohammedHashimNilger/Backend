f9b9567ee258ab2d29a54032161b233ab9589497
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-08-17 17:42:45 +0530

    more validation

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import Router from "express";
 import { registerUSer } from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
 
 const router = Router();
 
-router.route("/register").post(registerUSer);
+router.route("/register").post(userRegisterValidator(), validate, registerUSer);
 
 export default router;
--- Backend/src/validators/index.js
+++ Backend/src/validators/index.js
@@ -1,7 +1,27 @@
 import { body } from "express-validator";
 
 const userRegisterValidator = () => {
-  return [body("email").trim().notEmpty().withMessage];
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is reuired")
+      .isLowercase()
+      .withMessage("Username must be in lowercase")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters"),
+
+    body("password").trim().notEmpty().withMessage("Password is required"),
+
+    body("fullname").optional().trim(),
+  ];
 };
 
 export { userRegisterValidator };
